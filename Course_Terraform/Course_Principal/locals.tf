bcd2402e225f5e828cc2c8f9b872f8d03da01515
andre panizza <andresantoscloudaws@gmail.com>
2022-08-18 20:48:04 -0300

    update files terraform

---
+++ Course_Terraform/Course_Principal/locals.tf
@@ -0,0 +1,12 @@
+##Como a Azure não permite a utilização de tags, como a AWS, vamos utilizar o Locals para aficionar as Tags
+## dentro do Locals podemos colocar o nome da tag
+## para referenciar no bloco do Azure, no storaged-account.tf, utilizamos local.common_tags
+
+locals {
+
+    common_tags = {
+        owner = "AndrePanizza"
+        mangerd-by = "terraform"
+        }
+  
+}
--- Course_Terraform/Course_Principal/storaged-account.tf
+++ Course_Terraform/Course_Principal/storaged-account.tf
@@ -1,28 +1,29 @@
 ## novo arquivo para configuração da conta de storaged-account da Azure:
 
 resource "azurerm_resource_group" "first-resource-azurerm" {
-  name     = ""
+  name     = "storage_account_resource_group"
   location = var.location
 
-  tags = ""
+## adicionando o bloco do Locals como tag no azure adicionado
+  tags = local.common_tags
 }
 
 resource "azurerm_storage_account" "first-storage-azurerm" {
-  name                     = ""
-  resource_group_name      = ""
+  name                     = "andrepanizzadossantosstoragedaccount"
+  resource_group_name      = azurerm_resource_group.first-resource-azurerm.name
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 
-  tags = {
-    environment = ""
-  }
+  tags = local.common_tags
 }
 
 resource "azurerm_storage_container" "first-container-azurerm" {
-  name                  = ""
+  name                  = azurerm_storage_account.first-storage-azurerm.name
   storage_account_name  = ""
 }
-
+## importante, para referenciarmos um bloco, precisamos passar o tipo de resource ex: azurerm_resource_group.first-resource-azurerm.name
+## se ei quiser referenciar isso por exemplo no resource_group_name, eu posso fazer dessa forma acima
 ## Vamos fazendo a implementação dessa configuração e fazer o deploy disto.
 ## continuando a desenvolver toda infraestrutura!
+## 
